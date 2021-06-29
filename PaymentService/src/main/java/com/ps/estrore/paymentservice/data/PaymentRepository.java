7a58c1e7d51add1f410d2cbb557383a0a325111e
tarunpathak1996 <45016037+tarunpathak1996@users.noreply.github.com>
2021-06-29 20:52:39 +0530

    Delete PaymentService directory

--- PaymentService/src/main/java/com/ps/estrore/paymentservice/data/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ps.estrore.paymentservice.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<PaymentEntity, String> {
-	
-}
